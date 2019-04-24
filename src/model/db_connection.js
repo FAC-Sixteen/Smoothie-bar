a78c87fd02a3ad2580df9a201be9da2538181c97
Samantha Dickeson <samlucydickeson@gmail.com>
2019-04-24 16:51:16 +0100

    add comments to db

--- src/model/db_connection.js
+++ src/model/db_connection.js
@@ -4,10 +4,29 @@ require("env2")("./config.env");
 
 let connectionString = process.env.DATABASE_URL;
 
-// module.exports = new Pool({ connectionString });
+// Long version that explicitly sets all the Pool parameters:
+// ---
+// const params = url.parse(connectionString);
+// const [username, password] = params.auth.split(":");
 
+// const options = {
+//   host: params.hostname,
+//   port: params.port,
+//   database: params.pathname.split("/")[1],
+//   max: process.env.DB_MAX_CONNECTIONS || 2,
+//   user: username,
+//   password,
+//   ssl: params.hostname !== "localhost"
+// };
+
+// module.exports = new Pool(options);
+
+// middle version that explicitly sets some of the parameters:
 module.exports = new Pool({
   connectionString,
   max: 2,
   ssl: !connectionString.includes("localhost")
 });
+
+// super short version (that didn't work)
+// module.exports = new Pool({ connectionString });
